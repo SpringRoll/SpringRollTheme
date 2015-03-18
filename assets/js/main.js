062f878b1623c5eeb86133cd232c55a7c8f58d02
Matte Szklarz <matteszklarz@cloudkid.com>
2015-03-18 17:59:46 -0400

    tabs for docs section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
-$(":checkbox" ).click(function()
+var index = $('#index');
+var methods = $('#methods');
+var properties = $('#properties');
+var attributes = $('#attributes');
+var events = $('#events');
+
+var switchGroup = $(".switch-group");
+switchGroup.click(function()
 {
+    switchGroup.each(function() {
+        $(this).removeClass('active');
+    });
+    $(this).addClass('active');
     
+
+    var classes = this.className.split(/\s+/);
+    for (var i = 0, len = classes.length; i < len; i++)
+    {
+        var c = classes[i];
+        var switchTo = "switchTo__";
+        if (c.indexOf(switchTo) > -1)
+        {
+            switchClassDocTabs($('#' + c.substr(switchTo.length)));
+            return;
+        }
+    }
 });
+
+function switchClassDocTabs(switchTo)
+{
+    index.hide();
+    methods.hide();
+    properties.hide();
+    attributes.hide();
+    events.hide();
+
+    switchTo.show();
+}
